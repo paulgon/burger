67a0f5ec8f397418f5fd2011629c3ff7c01883ee
paulgonteacher <50177334+paulgon@users.noreply.github.com>
2019-08-09 09:45:23 -0500

    added db

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Cheeseburger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Veggie burger', FALSE);
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Parmesean burger', FALSE);
